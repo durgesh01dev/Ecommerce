cfb4ce55f7b47b1a58398502b2a4276a8999b10e
durgesh01dev <dev.durgesh01@gmail.com>
2022-11-04 20:56:37 +0530

    Update product by id, delete product by id, and get details of product by id are done.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,3 +18,66 @@ exports.getAllProducts = async (req, res) => {
     products,
   });
 };
+
+
+// Get one product details - 
+exports.getProductDetail = async(req, res,next) => {
+  const product = await Product.findById(req.params.id);
+  //check if product does not exist
+  if(!product){
+    res.status(500).json({
+      success: false,
+      message: `Product with id ${req.params.id} is not found`
+    });
+  }
+    //otherwise product with id exists
+    res.status(200).json({
+      success: true,
+      product
+    });
+}
+
+//function to update the product, only ADMIN can update Product details
+exports.updateProduct = async (req, res) => {
+  //using let so that same variable can updated
+  let product = await Product.findById(req.params.id);
+  //check if product with specified id is not found
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: `Product with id ${req.params.id} is not found`,
+    });
+  }
+  //product with id is found
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//function to delete the product based on id - ADMIN
+exports.deleteProduct = async(req, res) => {
+  const product = await Product.findById(req.params.id);
+  //check if product does not exist
+  if(!product){
+    return res.status(500).json({
+      success: false,
+      message: `Product with id ${req.params.id} is not found`,
+    });
+  }
+
+  //otherwise product is found, now delete that product
+  //works since product is still connected to the collection
+  await product.remove();
+  //return success now
+  res.status(200).json({
+    success: true, 
+    message: `Product with id ${req.params.id } is deleted successfully`
+  });
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const { getAllProducts, createProduct } = require('../controllers/productController');
+const express = require("express");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetail,
+} = require("../controllers/productController");
 
 const router = express();
 
 //route for the products
-router.route('/products').get(getAllProducts);
-router.route('/product/new').post(createProduct);
+router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router
+  .route("/product/:id")
+  .get(getProductDetail)
+  .put(updateProduct)
+  .delete(deleteProduct)
+  
 
 //exporting the router
-module.exports = router
+module.exports = router;
