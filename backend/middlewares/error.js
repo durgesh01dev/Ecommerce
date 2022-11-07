ddfebbd6edaa315c379423f3c049313d0a7fb37d
durgesh01dev <dev.durgesh01@gmail.com>
2022-11-07 12:26:53 +0530

    The CastError subjected to ID is explicitly handled within the error.js file

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,11 +1,20 @@
 // const ErrorHandler = require('../utils/errorhandler')
 
+const ErrorHandler = require("../utils/errorhandler");
+
 module.exports = (err, req, res, next) => {
     err.statusCode  = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
+
+    //handling invalid id error, like when casting id will generate error, for castError
+    if(err.name === 'CastError'){
+        const message = `Resource not found. Invalid: ${err.message}`;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         //to print the error details rather than status code use err.stack, for message use err.message
-        message: err.message
+        message: err.stack
     })
 }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const app = require("./app");
-const dotenv = require('dotenv');
-const connectDatabase = require('./config/database')
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+//handling uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
 
 //configuring details will be config file
 //first dotenv file is configured here
-dotenv.config({path:"backend/config/config.env"});
+dotenv.config({ path: "backend/config/config.env" });
 
 //now calling the connect function for databse
 connectDatabase();
@@ -18,14 +26,14 @@ const server = app.listen(process.env.PORT, () => {
  * Handling Promise Rejection scenarios
  * => works on unhandled Promise Rejection event
  * => Example, when mongodb url is incorrect
-*/
-process.on('unhandledRejection', err => {
+ */
+process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log('Shutting down server due to unhandled Promise Rejection');
+  console.log("Shutting down server due to unhandled Promise Rejection");
 
   //closing the server now
-  server.close(()  => {
+  server.close(() => {
     //as soon as server close, close all the processes and terminate
     process.exit(1);
-  })
-})
+  });
+});
